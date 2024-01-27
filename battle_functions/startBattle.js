7839c6ffd6471d5511b73dd734f7d62cfdff7203
SamuelCallWind <samcallwind@gmail.com>
2024-01-27 23:52:07 +0100

    Adding element in the movement container battle function

--- battle_functions/startBattle.js
+++ battle_functions/startBattle.js
@@ -6,16 +6,35 @@ function startBattle(enemyName) {
         throw new Error('Enemy');
     } else {
 
-        document.querySelector('.gameDisplay').style.backgroundImage = 'url(../images/loading_screens/fight_chicken.png)';
+        document.querySelector('.gameDisplay').style.backgroundImage = '../images/seaView.png';
         const movementContainer = document.querySelector('.movementContainer');
         movementContainer.classList.add('battle');
 
         movementContainer.style.filter = 'blur 50px';
-        movementContainer.style.opacity = '0';
-        movementContainer.style.backgroundColor = 'white';
+        while (movementContainer.firstChild) {
+            movementContainer.removeChild(movementContainer.firstChild);
+        }
 
         const enemyStrength = enemy.strength;
         const enemyHP = enemy["health points"];
+
+        const enemyName = enemy.name;
+
+        const nameText = document.createElement('div');
+        nameContainer.innerText = enemyName;
+        nameText.classList.add('nameText');
+
+        const strengthText = document.createElement('div');
+        nameContainer.innerText = enemyStrength;
+        strengthText.classList.add('strengthText');
+
+        const HPText = document.createElement('div');
+        nameContainer.innerText = enemyHP;
+        HPText.classList.add('HPText');
+
+        movementContainer.append(nameText, strengthText, HPText);
+        
+
 
     }
 }
