0602bad34d3cffe4c18deba0af5fcf8286c2c743
SamuelCallWind <Samcallwind@gmail.com>
2024-02-01 19:56:08 +0100

    Enemy removed whenhealth at 0 and is deleted

--- battle_functions/continueBattle.js
+++ battle_functions/continueBattle.js
@@ -1,10 +1,12 @@
 import { statsPlayer, textDisplayed } from "../main.js";
 import { redirectWeapon } from "../weapons/redirectWeapon.js";
 
+
 function continueBattle(enemy, chosenWeapon) {
     const enemyHealthInPoint = enemy.HP;
     const enemyGreenHealthBar = document.createElement('div');
 
+    enemyGreenHealthBar.innerText = enemyHealthInPoint;
     enemyGreenHealthBar.classList.add('enemyGreenHealthBar');
     document.querySelector('.enemyHPBar').appendChild(enemyGreenHealthBar)
     console.log(enemy.name, chosenWeapon);
@@ -16,7 +18,6 @@ function continueBattle(enemy, chosenWeapon) {
 }
 
 function handleAttack(enemy, chosenWeapon) {
-    console.log(chosenWeapon);
     let returnRandomNumber = (maxNumber) => Math.floor(Math.random() * maxNumber);
     const action = document.querySelector('.action');
     if (returnRandomNumber(100) < 1) {
@@ -28,25 +29,45 @@ function handleAttack(enemy, chosenWeapon) {
         action.style.display = 'none';
 
         buttonOK.addEventListener('click', function () {
-            action.style.display = 'flex';
             enemyAttack(enemy);
+            setTimeout(() => {
+                buttonOK.remove();
+                action.style.display = 'flex';
+            }, 300);
         })
     } else {
         let rawDamage = 2;
         const weaponAttributes = redirectWeapon(chosenWeapon);
-        console.log(weaponAttributes);
+        const enemyHP = document.querySelector('.enemyGreenHealthBar');
+        const playerCharacter = document.querySelector('.characterBackBattle');
 
+        console.log(`The enemy currently has ${document.querySelector('.enemyGreenHealthBar').innerText} health points`)
         if (chosenWeapon === null) {
+            playerCharacter.style.left = `${playerCharacter.style.left + 50}px`; 
             rawDamage = rawDamage + statsPlayer.strength;
+            enemyHP.innerText = parseInt(enemyHP.innerText) - parseInt(rawDamage);
         } else {
-            rawDamage = (rawDamage * statsPlayer.strength);
+            rawDamage = (rawDamage * statsPlayer.strength) + weaponAttributes.damage;
+            enemyHP.innerText = parseInt(enemyHP.innerText) - parseInt(rawDamage);
         }
+
+        if (enemyHP.innerText <= parseInt(0)) {
+            defeatEnemy(enemy);
+            textDisplayed.innerText = `You defeated the ${enemy.name}`;
+        }
+
     }
     
 }
 
 function enemyAttack() {
     console.log('The Enemy attacked');
+}
+
+function defeatEnemy() {
+    document.querySelector('.enemySprite').classList.add('fade');
+    document.querySelector('.enemyNameText').classList.add('fade');
+    document.querySelector('.enemyHPBar').classList.add('fade');
 }
 
 export { continueBattle };
